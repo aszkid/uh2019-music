1e5e0ce77876c3de57f6017e47b67e1eb108ef17
aszkid <polgomezriquelme@gmail.com>
2019-02-17 10:56:53 -0600

    Submit req

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 $("#ourform").submit(function(ev) {
+  ev.preventDefault();
   var chs = $("#chs").val();
   var len = $("#len").val();
   var temp = $("#temp").val();
-  $.get("localhost:80/", {chords: chs, length: len, temperature: temp}, function(data) {
+  $.get("http://localhost:80/gen", {chords: chs, length: len, temperature: temp}, function(data) {
     alert(data);
   });
-  alert("done");
 });
