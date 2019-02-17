c74d706e33daca45d5056705de283f930754913d
Ted Kim <tedkim97@gmail.com>
2019-02-17 10:08:48 -0600

    Updated server

--- server.py
+++ server.py
@@ -1,10 +1,22 @@
-from flask import Flask 
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def main():
-	return "This is Pol's and Ted's Uncommon Hacks Project!" 
+	return 'Howdy ;0 '
+	# return render_template('index.html')
+
+@app.route('/temperature=<temp>/lchs=<chords>/length=<len>',methods=['GET'])
+def show_stuff(temp,chords,len):
+	return "temperature = {} lchs = {} len = {}".format(temp,chords,len)
+
+
 
 if __name__ == "__main__":
 	app.run(debug=True, host="0.0.0.0", port=80)
+
+
+#Parameter with list of chords 
+#Temperature 
+#length of the generated chords 
